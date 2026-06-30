f6d78997e8036447186dfb42fc6b32e13ea9666d
Claire Morand <claire@cmswengcy.co.uk>
2026-06-30 12:50:39 +0200

    Add CMake options to enable/disable Widgets and QML frontends independently

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,53 @@
-#include <QApplication>
+#ifdef ENABLE_QML
 #include <QQmlContext>
 #include <QQmlApplicationEngine>
+#endif
+
+#ifdef ENABLE_WIDGETS
 #include "ScatterWidget.h"
+#include <QApplication>
+#else
+#include <QGuiApplication>
+#endif
+
 #include "ScatterData.h"
 
 int main(int argc, char *argv[])
 {
-  QApplication app(argc, argv);
+#ifdef ENABLE_WIDGETS
+    QApplication app(argc, argv);
+#else
+    QGuiApplication app(argc, argv);
+#endif
 
   // Backend. Single instance shared between Qt Widgets and QML frontends
   ScatterData data;
 
+#ifdef ENABLE_QML
   // QML engine. context must be set before loading QML,
     // so signals emitted during data loading are received by QML
   QQmlApplicationEngine engine;
   engine.rootContext()->setContextProperty("scatterData", &data);
   engine.load(QUrl("qrc:/ScivisQt/qml/Main.qml"));
+#endif
 
+#ifdef ENABLE_WIDGETS
   // Qt Widgets frontend
   ScatterWidget widget(&data);
   widget.setWindowTitle("ScivisQt - Widgets");
+#endif
 
   // Load data after both frontends are ready
   data.loadModel("../models/model.pt");
   data.loadData("../data/points.csv");
 
+
+#ifdef ENABLE_WIDGETS
   widget.show();
+#endif
 
   // Quit application when last window is closed
-  QObject::connect(&app, &QApplication::lastWindowClosed, &app, &QApplication::quit);
+  QObject::connect(&app, &QGuiApplication::lastWindowClosed, &app, &QCoreApplication::quit);
   
   return app.exec();
 }
