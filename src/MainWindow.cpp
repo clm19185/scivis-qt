0cf4e3bbbd14d66a916bd516e0b34975186d7e67
Claire Morand <claire@cmswengcy.co.uk>
2026-07-03 17:38:33 +0200

    Add presets to toolbar for loading models/data from MainWindow

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -2,6 +2,7 @@
 #include <QToolBar>
 #include <QFileDialog>
 #include <QCoreApplication>
+#include <QStatusBar>
 
 MainWindow::MainWindow(ScatterData* data, QWidget* parent)
   : QMainWindow(parent),
@@ -15,25 +16,61 @@ MainWindow::MainWindow(ScatterData* data, QWidget* parent)
 
 void MainWindow::setupToolbar()
 {
-    QToolBar* toolbar = addToolBar("Main");
+  QToolBar* toolbar = addToolBar("Main");
     
-    QAction* loadModelAction = toolbar->addAction("Load Model");
-    QAction* loadDataAction  = toolbar->addAction("Load Data");
+  QAction* loadModelAction = toolbar->addAction("Load Model");
+  QAction* loadDataAction  = toolbar->addAction("Load Data");
     
-    connect(loadModelAction, &QAction::triggered, this, &MainWindow::onLoadModel);
-    connect(loadDataAction,  &QAction::triggered, this, &MainWindow::onLoadData);
+  connect(loadModelAction, &QAction::triggered, this, &MainWindow::onLoadModel);
+  connect(loadDataAction,  &QAction::triggered, this, &MainWindow::onLoadData);
+
+  toolbar->addSeparator();
+
+  // Preset actions
+  QAction* presetSeparated    = toolbar->addAction("Separated");
+  QAction* presetOverlapping  = toolbar->addAction("Overlapping");
+  QAction* presetMoons        = toolbar->addAction("Moons");
+  QAction* presetThreeClasses = toolbar->addAction("3 Classes");
+
+  connect(presetSeparated,    &QAction::triggered, this, [this]{ loadPreset("separated"); });
+  connect(presetOverlapping,  &QAction::triggered, this, [this]{ loadPreset("overlapping"); });
+  connect(presetMoons,        &QAction::triggered, this, [this]{ loadPreset("moons"); });
+  connect(presetThreeClasses, &QAction::triggered, this, [this]{ loadPreset("three_classes"); });
 }
 
 void MainWindow::onLoadModel()
 {
-  QString path = QFileDialog::getOpenFileName(this, "Load Model", QCoreApplication::applicationDirPath() + "/../models", "TorchScript Model (*.pt)");
-  if (path.isEmpty()) return; // dialog cancelled
+  QString path = QFileDialog::getOpenFileName(
+     this,
+     "Load Model",
+     "../models",
+     "TorchScript models (*.pt)"
+  );
+  if (path.isEmpty()) return; 
   m_data->loadModel(path);
+  QFileInfo fileInfo(path);
+  statusBar()->showMessage("Model: " + fileInfo.fileName());
 }
 
 void MainWindow::onLoadData()
 {
-  QString path = QFileDialog::getOpenFileName(this, "Load Data", QCoreApplication::applicationDirPath() + "/../data", "CSV Files (*.csv)");
-  if (path.isEmpty()) return; // dialog cancelled
+  QString path = QFileDialog::getOpenFileName(
+     this,
+     "Load Data",
+     "../data",
+     "CSV Files (*.csv)"
+  );
+  if (path.isEmpty()) return; 
   m_data->loadData(path);
+  QFileInfo fileInfo(path);
+  statusBar()->showMessage("Data: " + fileInfo.fileName());
 }
+
+void MainWindow::loadPreset(const QString& name)
+{
+  m_data->loadModel("../models/" + name + ".pt");
+  m_data->loadData("../data/" + name + ".csv");
+  statusBar()->showMessage("Preset: " + name);
+}
+
+
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -8,18 +8,19 @@
 // Owns the toolbar and wraps ScatterWidget as the central widget.
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit MainWindow(ScatterData* data, QWidget* parent = nullptr);
+  explicit MainWindow(ScatterData* data, QWidget* parent = nullptr);
 
 private:
-    ScatterData*   m_data;
-    ScatterWidget* m_scatterWidget;
+  ScatterData*   m_data;
+  ScatterWidget* m_scatterWidget;
     
-    void setupToolbar();
+  void setupToolbar();
+  void loadPreset(const QString& name);
 
 private Q_SLOTS:
-    void onLoadModel();
-    void onLoadData();
+  void onLoadModel();
+  void onLoadData();
 };
